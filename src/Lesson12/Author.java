c9b8871399dc917d0a5c3d10d26ded4d290d9dba
OlegYakunin <olegspb7@gmail.com>
2023-05-18 10:58:18 +0300

    HW is ready!

--- src/Lesson12/Author.java
+++ src/Lesson12/Author.java
@@ -1,5 +1,7 @@
 package Lesson12;
 
+import java.util.Objects;
+
 public class Author {
     private final String firstName;
     private final String lastName;
@@ -14,5 +16,25 @@ public class Author {
 
     public String getLastName() {
         return this.lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null) return false;
+        if (this == o) return true;
+        if (!o.getClass().equals(Author.class)) return false;
+        Author other = (Author) o;
+        return Objects.equals(this.firstName, other.firstName)
+                && Objects.equals(this.lastName, other.lastName);
+
+    }
+    @Override
+    public int hashCode () {
+        return Objects.hash(this.firstName, this.lastName);
+    }
+
+    @Override
+    public String toString() {
+        return this.firstName + " " + this.lastName;
     }
 }
